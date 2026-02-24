ea50dac92926549e3efc2344e125fb9ceaf556d9
Thriveedh <thundurthythriveedh@gmail.com>
2026-02-25 01:45:49 +0530

    Add files via upload

---
+++ src/DeepQ/train_double_dqn.py
@@ -0,0 +1,140 @@
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import random
+
+from env import PartitionEnv
+from dqn_model import DQN
+from replay_buffer import ReplayBuffer
+
+items = np.loadtxt(
+    r"C:\Users\Thriveedh\Downloads\mdtwnpp_500_20a.txt",
+    skiprows=1
+)
+
+scale=items.max(axis=0)
+items = items / items.max(axis=0)
+
+env = PartitionEnv(items)
+
+state_size = 1 + items.shape[1]
+action_size = 2
+
+model = DQN(state_size, action_size)
+target_model = DQN(state_size, action_size)
+
+target_model.load_state_dict(model.state_dict())
+target_model.eval()
+
+optimizer = optim.Adam(model.parameters(), lr=0.0003)
+loss_fn = nn.MSELoss()
+
+# Replay Buffer
+buffer = ReplayBuffer(capacity=20000)
+batch_size = 64
+
+#Hyper Parameters
+gamma = 0.99
+
+epsilon = 1.0
+epsilon_decay = 0.9995
+epsilon_min = 0.1
+
+episodes = 1000
+target_update_freq = 50
+
+# Early stopping
+best_reward = float("-inf")
+patience = 600
+no_improve = 0
+
+for episode in range(episodes):
+
+    state = torch.tensor(env.reset(), dtype=torch.float32)
+    total_reward = 0
+
+    while True:
+
+        # epsilon-greedy
+        if random.random() < epsilon:
+            action = random.randint(0, 1)
+        else:
+            with torch.no_grad():
+                action = torch.argmax(model(state)).item()
+
+        next_state, reward, done = env.step(action)
+        next_state = torch.tensor(next_state, dtype=torch.float32)
+
+        buffer.push(state, action, reward, next_state, done)
+
+        state = next_state
+        total_reward += reward
+
+        # Double dqn
+        if len(buffer) >= batch_size:
+
+            states, actions, rewards, next_states, dones = buffer.sample(batch_size)
+
+            q_values = model(states).gather(1, actions.unsqueeze(1)).squeeze(1)
+
+            with torch.no_grad():
+
+                # MAIN network selects best action
+                next_actions = model(next_states).argmax(1)
+
+                # TARGET network evaluates that action
+                next_q = target_model(next_states).gather(
+                    1, next_actions.unsqueeze(1)
+                ).squeeze(1)
+
+            targets = rewards + gamma * next_q * (1 - dones)
+
+            loss = loss_fn(q_values, targets)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if done:
+            break
+
+    epsilon = max(epsilon * epsilon_decay, epsilon_min)
+
+    if episode % target_update_freq == 0:
+        target_model.load_state_dict(model.state_dict())
+
+    # Early stopping + best model saving
+    if total_reward > best_reward + 1:
+        best_reward = total_reward
+        no_improve = 0
+        torch.save(model.state_dict(), "best_double_dqn.pth")
+    else:
+        no_improve += 1
+
+    if episode % 100 == 0:
+        print(f"Episode {episode}, Reward: {total_reward:.2f}")
+
+    if no_improve >= patience:
+        print(f"\nEarly stopping at episode {episode}")
+        break
+
+
+model.load_state_dict(torch.load("best_double_dqn.pth"))
+
+env.reset()
+partition = []
+
+for _ in range(len(items)):
+    state = torch.tensor(env._get_state(), dtype=torch.float32)
+    action = torch.argmax(model(state)).item()
+    partition.append(action)
+    env.step(action)
+
+print("\nFinal Result")
+print("Items:\n", items)
+print("Partition:", partition)
+# print("Sum A:", env.sumA)
+# print("Sum B:", env.sumB)
+print("Final imbalance:", np.abs(env.sumA - env.sumB))
+print("Max imbalance:", np.max(np.abs(env.sumA - env.sumB)))
---
+++ src/Sarsa/sarsa.py
@@ -0,0 +1,95 @@
+import numpy as np
+import random
+from collections import defaultdict
+
+items = np.loadtxt(
+    r"C:\Users\Thriveedh\Downloads\mdtwnpp_500_20a.txt",
+    skiprows=1
+)
+n, dim = items.shape
+scale = items.max(axis=0)
+items=items/scale
+
+Q = defaultdict(lambda: np.zeros(2))
+
+# Hyper Parameters
+alpha = 0.1
+gamma = 0.9
+epsilon = 1.0
+epsilon_decay = 0.995
+epsilon_min = 0.05
+episodes = 6000
+
+# State Representation
+def get_state(index, sumA, sumB):
+    diff = sumA - sumB
+    buckets = tuple(int(np.sign(d)) for d in diff)
+    return (index,) + buckets
+
+# ε-greedy function 
+def choose_action(state):
+    if random.random() < epsilon:
+        return random.randint(0, 1)
+    return np.argmax(Q[state])
+
+
+# Training Loop
+for ep in range(episodes):
+
+    sumA = np.zeros(dim)
+    sumB = np.zeros(dim)
+    index = 0
+
+    state = get_state(index, sumA, sumB)
+    action = choose_action(state)  
+
+    while index < n:
+        if action == 0:
+            sumA += items[index]
+        else:
+            sumB += items[index]
+
+        if index == n - 1:
+            reward = -np.max(np.abs(sumA - sumB))
+        else:
+            reward = 0
+
+        next_state = get_state(index + 1, sumA, sumB)
+        next_action = choose_action(next_state)
+
+        Q[state][action] += alpha * (
+            reward + gamma * Q[next_state][next_action] - Q[state][action]
+        )
+
+        state = next_state
+        action = next_action
+        index += 1
+
+    epsilon = max(epsilon * epsilon_decay, epsilon_min)
+
+    if ep % 500 == 0:
+        print(f"Episode {ep} completed")
+
+
+# Final Greedy Policy
+sumA = np.zeros(dim)
+sumB = np.zeros(dim)
+partition = []
+
+for i in range(n):
+    state = get_state(i, sumA, sumB)
+    action = np.argmax(Q[state])
+    partition.append(action)
+
+    if action == 0:
+        sumA += items[i]
+    else:
+        sumB += items[i]
+
+print("\nFinal Result")
+print("Items:\n", items)
+print("Partition (0=A, 1=B):", partition)
+# print("Sum A:", sumA*scale)
+# print("Sum B:", sumB*scale)
+print("Final imbalance:", np.abs(sumA - sumB))
+print("Max imbalance:", np.max(np.abs(sumA - sumB)))
